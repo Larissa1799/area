3bdbebbd7245bfc062363016455907e691288993
LARISSA MATTOS DE MARTINI <109092221+Larissa1799@users.noreply.github.com>
2022-07-20 09:02:46 -0300

    Add files via upload

---
+++ variaveis.java
@@ -0,0 +1,41 @@
+package dados;
+
+public class variaveis {
+
+	public static void area(double lado) 
+		 {
+
+	        System.out.println("Área do quadrado:" + lado * lado);
+	    }
+
+	    public static void area(double lado1, double lado2) {
+
+	        System.out.println("Área do retângulo:" + lado1 * lado2);
+	    }
+
+	    public static void area(double baseMaior, double baseMenor, double altura) {
+
+	        System.out.println("Área do trapézio:" + ((baseMaior+baseMenor)*altura) / 2);
+	    }
+
+	    public static void area(float diagonal1, float diagonal2) {
+
+	        System.out.println("Área do losango:" + (diagonal1 * diagonal2)/2);
+	    }
+
+	   
+
+	    	public static void main(String[] args) {
+
+	            // Quadrilátero
+	            System.out.println("Exercício quadrilátero");
+	            Quadrilatero.area(3);
+	            Quadrilatero.area(5d,5d);
+	            Quadrilatero.area(7,8,9);
+	            Quadrilatero.area(5f,5f);
+
+	        }
+	    
+	}
+
+
